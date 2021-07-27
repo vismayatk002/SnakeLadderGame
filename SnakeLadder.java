764183de33a3831d9757557eee03a76e67e26768
vismayatk002 <vismayatk002@gmail.com>
2021-07-27 22:05:33 +0530

    Updated with Rolling die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,8 +1,25 @@
+import java.util.*;  
+import java.lang.*;
 public class SnakeLadder{
 
     public static void main(String[] args){
 
+        Scanner sc= new Scanner(System.in);
         //initialise position as 0
         int position = 0;
+        int min = 1;
+        int max = 6;
+        System.out.println("Press 1 for Roll the Die :");
+        int number = sc.nextInt();
+        if(number == 1){
+
+            int dieNumber = (int)(Math.random() * (max - min +1) + min);
+            System.out.println("Dies number : " + dieNumber);
+        }
+        else{
+
+            System.out.println("Not Valid");
+
+        }
     }
 }
