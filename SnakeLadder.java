1f038ae309975927b22277c24389cea6db14bb77
vismayatk002 <vismayatk002@gmail.com>
2021-07-27 23:17:19 +0530

    Updated with option

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -12,13 +12,27 @@ public class SnakeLadder{
         System.out.println("Press 1 for Roll the Die :");
         int number = sc.nextInt();
         if(number == 1){
-
+            // Get dies number
             int dieNumber = (int)(Math.random() * (max - min +1) + min);
             System.out.println("Dies number : " + dieNumber);
+            // Get options 
+            int checkOption = (int)(Math.random() * 10) % 3;
+            
+            switch(checkOption){
+                case 0 : 
+                    System.out.println("Option : No play ");
+                break;
+                case 1 : 
+                    System.out.println("Option : Ladder ");
+                break;
+                case 2 : 
+                    System.out.println("Option : Snake ");
+                break;
+            }
         }
         else{
 
-            System.out.println("Not Valid");
+            System.out.println("Invalid Input");
 
         }
     }
